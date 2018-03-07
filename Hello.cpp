2c449d0af5f3a6b080414909763c57547d23779e
Jennifer Loe <Jennifer_loe@baylor.edu>
2018-03-07 14:56:31 -0600

    Have a Hello World File.

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+//This program is by Jennifer Loe
+// Remembering how to write c++ code from scratch 
+// for debugging.
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
